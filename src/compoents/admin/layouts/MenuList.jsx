654ee825fa6d1be7c8df9162a336a48a3ef6fffc
Comartsau <comartsau@gmail.com>
2024-02-11 12:52:34 +0700

    update

--- src/compoents/admin/layouts/MenuList.jsx
+++ src/compoents/admin/layouts/MenuList.jsx
@@ -1,9 +1,14 @@
 import { Menu, ConfigProvider } from "antd";
 import { DatabaseOutlined } from "@ant-design/icons";
 import { TbReportAnalytics } from "react-icons/tb";
-import { Link , useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+import { useEffect } from "react";
 
 import { GoWorkflow } from "react-icons/go";
+
+import { collapsedStore } from "../../../store/Store";
+import { useRecoilState } from "recoil";
 
 // eslint-disable-next-line no-unused-vars, react/prop-types
 const MenuList = ({ darkTheme, toggleTheme }) => {
@@ -11,6 +16,8 @@ const MenuList = ({ darkTheme, toggleTheme }) => {
   const currentPath = window.location.pathname;
 
   const iconStyle = { fontSize: "26px", marginRight: "5px", marginTop: "7px" };
+
+  const [collapsed, setCollapsed] = useRecoilState(collapsedStore);
 
   return (
     <div>
@@ -29,20 +36,26 @@ const MenuList = ({ darkTheme, toggleTheme }) => {
           className="menu-bar"
           selectedKeys={[currentPath]}
         >
-          <Menu.Item
-            key="/admin"
-            icon={<DatabaseOutlined style={iconStyle} />}
-          >
-            <Link to="/admin">ข้อมูลพื้นฐาน</Link>
+          <Menu.Item key="/admin" icon={<DatabaseOutlined style={iconStyle} />}>
+            <Link to="/admin" onClick={() => setCollapsed(!collapsed)}>
+              ข้อมูลพื้นฐาน
+            </Link>
           </Menu.Item>
-          <Menu.Item key="/admin/process" icon={<GoWorkflow style={iconStyle} />}>
-            <Link to="/admin/process">Process</Link>
+          <Menu.Item
+            key="/admin/process"
+            icon={<GoWorkflow style={iconStyle} />}
+          >
+            <Link to="/admin/process" onClick={() => setCollapsed(!collapsed)}>
+              Process
+            </Link>
           </Menu.Item>
           <Menu.Item
             key="/admin/report"
             icon={<TbReportAnalytics style={iconStyle} />}
           >
-            <Link to="/admin/report">รายงาน</Link>
+            <Link to="/admin/report" onClick={() => setCollapsed(!collapsed)}>
+              รายงาน
+            </Link>
           </Menu.Item>
         </Menu>
       </ConfigProvider>
