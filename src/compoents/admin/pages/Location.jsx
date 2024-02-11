0b38de039f21d923bce6b345d41a516d6925264f
Comartsau <comartsau@gmail.com>
2024-02-11 12:05:38 +0700

    update

--- src/compoents/admin/pages/Location.jsx
+++ src/compoents/admin/pages/Location.jsx
@@ -190,7 +190,7 @@ const Location = () => {
           </div>
         </div>
         {/* ------------ table  ----------------------------------------- */}
-        <Card className="mt-5 w-full h-[42vh] sm:h-[48vh] md:h-[58vh] lg:h-[60vh] overflow-auto  border-2  ">
+        <Card className="mt-5 w-full h-[40vh] sm:h-[48vh] md:h-[58vh] lg:h-[60vh] overflow-auto  border-2  ">
           <div>
             <table className="w-full min-w-max  ">
               <thead>
